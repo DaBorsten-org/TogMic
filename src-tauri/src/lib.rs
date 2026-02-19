d655877b19eb6e9b34fbf9809a8e8559f85fb7aa
DaBorsten <boas.rautmann@gmail.com>
2026-02-19 19:51:32 +0100

    feat: update release workflow to trigger on version tags and add single-instance plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -423,6 +423,7 @@ pub fn run() {
     };
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|_app, _argv, _cwd| {}))
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::new().build())
